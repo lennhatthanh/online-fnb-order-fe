b703ffc42f805ec4622a3149fb9d783c5040d3f3
Lê Nguyễn Nhật Thành <lenguyennhatthanh72@gmail.com>
2025-12-19 18:41:23 +0700

    Implement code changes to enhance functionality and improve performance

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,45 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: "autoUpdate", // Tự động cập nhật khi có nội dung mới
+            includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+            manifest: {
+                name: "Food Online Delivery",
+                short_name: "Foodly",
+                description: "Ứng dụng đặt món ăn trực tuyến nhanh chóng, tiện lợi và an toàn.",
+                theme_color: "#ff6b01",
+                background_color: "#ffffff",
+                display: "standalone",
+                icons: [
+                    {
+                        src: "/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/pwa-192x192.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
+    },
+    server: {
+        host: true, // hoặc "0.0.0.0"
+        port: 5173,
     },
 });
